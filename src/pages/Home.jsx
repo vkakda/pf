2876f01f546678da0f32a49eaf2a0a400d2c8880
vkakda <vjcse12@gmail.com>
2025-10-28 21:23:42 +0530

    add some updates in hero section and fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { profile } from '../data/profile'
 import { useReveal } from '../hooks/useReveal'
+import {Typewriter} from 'react-simple-typewriter'
+
 
 
 export default function Home() {
@@ -9,8 +11,33 @@ export default function Home() {
       <div className="hero-text">
 
 
-        <h1 className="display">{profile.name}</h1>
-        <p className="subtitle">{profile.title}</p>
+        <h1 className="display"><span className="intro-text">Hi I'm </span>
+        {profile.name}</h1>
+        <p className="subtitle font-bold"><Typewriter
+                words={[
+  'MERN Full-Stack Developer',
+  'Web Application Developer',
+  'AI Enthusiast',
+  'Creative Frontend Designer',
+  'Digital Storyteller',
+  'React.js Developer',
+  'Node.js & Express Developer',
+  'MongoDB Specialist',
+  'Tailwind CSS & Modern UI',
+  'Building Scalable Web Experiences',
+  'Turning Ideas into Code',
+  'Crafting Interactive Interfaces',
+  'Driven by Simplicity, Inspired by Depth'
+]}
+
+                loop={0}
+                cursor
+                cursorStyle="|"
+                typeSpeed={50}
+                deleteSpeed={50}
+                delaySpeed={2000}
+              />
+              </p>
         <p className="summary">{profile.summary}</p>
         <div className="cta-row">
           <a className="btn primary" href="#projects">View Projects</a>
